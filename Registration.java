3cc84d372dca559d8398e9683f6378c6e758dab5
Phenyo Mthethwa <st10527383@rcconnect.edu.za>
2026-04-15 13:28:05 +0200

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.login;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+ public class Registration {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Login user = new Login();
+
+        int choice;
+
+        do {
+            System.out.println("\n=== MENU ===");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.print("Choose option: ");
+
+            choice = sc.nextInt();
+            sc.nextLine(); // FIX for Scanner (important!)
+
+            switch (choice) {
+                case 1:
+                    user.register(sc);
+                    break;
+
+                case 2:
+                    user.login(sc);
+                    break;
+
+                case 3:
+                    System.out.println("Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+
+        } while (choice != 3);
+
+        sc.close();
+    }
+    
+
