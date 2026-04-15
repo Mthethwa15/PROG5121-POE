eca63f91f735b80ac6fea267139f09e76cacf5c7
Phenyo Mthethwa <st10527383@rcconnect.edu.za>
2026-04-15 13:41:57 +0200

    Remove commented license and clean up Registration.java

--- Registration.java
+++ Registration.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.login;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Student
- */
+
  public class Registration {
     public static void main(String[] args) {
 
